07ec7375dbde9aa133af6e66f724b3bc79f1df53
nagasai-web <nagasaimadaparthi@gmail.com>
2026-05-05 19:25:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearAll() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
